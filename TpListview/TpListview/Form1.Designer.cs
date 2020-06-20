a6b88a2d8cc3867a7c58f9c09502b76860ddf85d
Rida Rhnizar <ridarhnizar1337@gmail.com>
2020-06-21 00:29:09 +0100

    premiere

---
+++ TpListview/TpListview/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace TpListview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.butSupp = new System.Windows.Forms.Button();
+            this.butMod = new System.Windows.Forms.Button();
+            this.buttQuitter = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(360, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            700,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(74, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Facture n°";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.ForeColor = System.Drawing.Color.Red;
+            this.groupBox1.Location = new System.Drawing.Point(45, 85);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 148);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ARTICLE";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(16, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Quantité";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(16, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Prit Unitair HT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(16, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Designation";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(538, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(217, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TAUX TVA";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 73);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "20%";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "7%";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Magenta;
+            this.button1.Location = new System.Drawing.Point(538, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(665, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Effacer";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.listView1.Location = new System.Drawing.Point(45, 244);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(855, 216);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // butSupp
+            // 
+            this.butSupp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.butSupp.Location = new System.Drawing.Point(45, 466);
+            this.butSupp.Name = "butSupp";
+            this.butSupp.Size = new System.Drawing.Size(90, 34);
+            this.butSupp.TabIndex = 7;
+            this.butSupp.Text = "Supprimer";
+            this.butSupp.UseVisualStyleBackColor = false;
+            this.butSupp.Click += new System.EventHandler(this.butSupp_Click);
+            // 
+            // butMod
+            // 
+            this.butMod.BackColor = System.Drawing.Color.Green;
+            this.butMod.Location = new System.Drawing.Point(383, 466);
+            this.butMod.Name = "butMod";
+            this.butMod.Size = new System.Drawing.Size(90, 34);
+            this.butMod.TabIndex = 8;
+            this.butMod.Text = "Modifier";
+            this.butMod.UseVisualStyleBackColor = false;
+            this.butMod.Click += new System.EventHandler(this.butMod_Click);
+            // 
+            // buttQuitter
+            // 
+            this.buttQuitter.BackColor = System.Drawing.Color.Red;
+            this.buttQuitter.Location = new System.Drawing.Point(810, 466);
+            this.buttQuitter.Name = "buttQuitter";
+            this.buttQuitter.Size = new System.Drawing.Size(90, 34);
+            this.buttQuitter.TabIndex = 9;
+            this.buttQuitter.Text = "Quitter";
+            this.buttQuitter.UseVisualStyleBackColor = false;
+            this.buttQuitter.Click += new System.EventHandler(this.buttQuitter_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 528);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total Des Mt (HT)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 571);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Total Des MTS (TVA)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 610);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Net à payer";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(251, 528);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(155, 22);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(251, 568);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(155, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(251, 610);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(155, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(1375, 645);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttQuitter);
+            this.Controls.Add(this.butMod);
+            this.Controls.Add(this.butSupp);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Facture";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button butSupp;
+        private System.Windows.Forms.Button butMod;
+        private System.Windows.Forms.Button buttQuitter;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ TpListview/TpListview/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TpListview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ViderForm()
+        {
+            textBox1.Text = textBox2.Text = textBox3.Text = "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double q = double.Parse(textBox2.Text);
+            double p = double.Parse(textBox3.Text);
+            double MHT = q * p;
+            ListViewItem item = new ListViewItem(textBox1.Text);
+            item.SubItems.Add(textBox2.Text);
+            item.SubItems.Add(textBox3.Text);
+            item.SubItems.Add(MHT.ToString());
+            if (radioButton1.Checked == true)
+            {
+                p = 0.07;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                p = 0.2;
+            }
+            else
+                MessageBox.Show("Vérifier les champs");
+            item.SubItems.Add((MHT * p).ToString());
+            item.SubItems.Add((MHT + p * MHT).ToString());
+            listView1.Items.Add(item);
+            double THTC = 0;
+            double TTVA = 0;
+            for (int i = 0; i < listView1.Items.Count;i++ )
+            {
+                THTC += Convert.ToDouble(listView1.Items[i].SubItems[3].Text);
+                TTVA += Convert.ToDouble(listView1.Items[i].SubItems[4].Text);
+            }
+            textBox4.Text = THTC.ToString()+ " $";
+            textBox5.Text = TTVA.ToString()+ " $";
+            textBox6.Text = (TTVA + THTC).ToString() + " $";
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Width = 730;
+            listView1.Columns.Add("Designation", 120);
+            listView1.Columns.Add("Prix Unitaire", 120);
+            listView1.Columns.Add("Quantité", 120);
+            listView1.Columns.Add("Montant ht", 120);
+            listView1.Columns.Add("Montant TVA", 120);
+            listView1.Columns.Add("Total(TTC)", 120);
+            listView1.View = View.Details;
+         
+        }
+
+        private void buttQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void butSupp_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+                if (item.Selected) 
+                    listView1.Items.Remove(item);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ViderForm();
+        }
+
+        private void butMod_Click(object sender, EventArgs e)
+        {
+           if (listView1.SelectedIndices.Count>0)
+           {
+               int i = listView1.SelectedIndices[0];
+               textBox1.Text = listView1.Items[i].SubItems[0].Text;
+               textBox2.Text = listView1.Items[i].SubItems[1].Text;
+               textBox3.Text = listView1.Items[i].SubItems[2].Text;
+               if (Convert.ToDouble(listView1.Items[i].SubItems[3].Text) == 20 * Convert.ToDouble(listView1.Items[i].SubItems[4].Text))
+               {
+                   radioButton2.Checked = true;
+               }
+               else
+                   radioButton1.Checked = true;
+               listView1.Items[i].Remove();
+           }
+                    
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
